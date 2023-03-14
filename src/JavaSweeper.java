d279c000a4f2cee05f32ab18c3baaa8826055000
Hrebenyuk Oleksii <grebenuka147it@gmail.com>
2023-03-14 12:02:33 +0200

    Initial Panel

--- src/JavaSweeper.java
+++ src/JavaSweeper.java
@@ -1,15 +1,33 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class JavaSweeper extends JFrame {
+
+    private JPanel panel;
+
     public static void main(String[] args) {
         new JavaSweeper();
     }
 
-    public JavaSweeper() {
+    private JavaSweeper() {
+        initPanel();
+        initFrame();
+    }
+
+    private void initPanel() {
+        panel = new JPanel();
+        panel.setPreferredSize(new Dimension(500, 300));
+        add(panel);
+    }
+
+    private void initFrame() {
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setTitle("Java Sweeper");
-        setLocationRelativeTo(null);
         setVisible(true);
         setResizable(false);
+        pack();
+        setLocationRelativeTo(null);
     }
+
+
 }
